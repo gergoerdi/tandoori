46212eaa1c18283d02f4ac489be5f05978abff15
Dr. ERDI Gergo <gergo@erdi.hu>
2009-09-13 13:21:15 +0200

    Nice-ify types in error messages before printing them

--- sandbox.hs
+++ sandbox.hs
@@ -8,14 +8,29 @@ import Tandoori.Ty.Infer
 import Tandoori.Test
 import Language.Haskell.Syntax
 import Language.Haskell.Pretty
-
+import Tandoori.Errors
+    
 test expr = do (_, ty) <- infer emptyPoly expr                     
                errors <- getErrors
                return (ty, errors)
     
 main' = do expr <- getTestExpr
-           let (ty, errors) = evalState (test expr) newState
+           let (ty, errors) = evalState (niceM $ evalState (test expr) newState) newTyPrinter
            mapM print errors
-           return $ niceTy $ ty
+           return $ ty
 
+    where niceM (ty, errors) = do errors' <- mapM niceErrorM errors
+                                  ty' <- niceTyM ty
+                                  return (ty', errors')
+                  
+niceErrorM (ErrorMessage loc expr content) = liftM (ErrorMessage loc expr) (niceContentM content)
+niceContentM (UnificationFailed ms typairs) = do let ms' = ms
+                                                 typairs' <- mapM niceTyPairM typairs
+                                                 return $ UnificationFailed ms' typairs'
+    where niceTyPairM (l, r) = do l' <- niceTyM l
+                                  r' <- niceTyM r
+                                  return (l', r')
+niceContentM content = return $ content
+                                              
+                                             
 main = liftM prettyPrint main'
